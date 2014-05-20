c1a3d005782acd2e90169fbd543ad8e00c353af8
Joshua Kalpin <t-jokal@microsoft.com>
2014-05-19 17:16:35 -0700

    Add Roles features to the sample.
    Only supporting listing roles, not adding users to a role. Also some other
    UI cleanup included

---
+++ WebAppGraphAPI/Controllers/RolesController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using WebAppGraphAPI.Utils;
+
+namespace WebAppGraphAPI.Controllers
+{
+    [Authorize]
+    public class RolesController : Controller
+    {
+
+        private const string TenantIdClaimType = "http://schemas.microsoft.com/identity/claims/tenantid";
+        private string graphResourceId = ConfigurationManager.AppSettings["ida:GraphUrl"];
+        private static string graphApiVersion = ConfigurationManager.AppSettings["ida:GraphApiVersion"];
+
+        /// <summary>
+        /// Gets a list of <see cref="Role"/> objects from Graph.
+        /// </summary>
+        /// <returns>A view with the list of <see cref="Role"/> objects.</returns>
+        public ActionResult Index()
+        {
+            string accessToken = null;
+            string tenantId = ClaimsPrincipal.Current.FindFirst(TenantIdClaimType).Value;
+            if (tenantId != null)
+            {
+                accessToken = WebAppGraphAPI.Utils.TokenCacheUtils.GetAccessTokenFromCacheOrRefreshToken(tenantId, graphResourceId);
+            }
+            if (accessToken == null)
+            {
+                //
+                // If refresh is set to true, the user has clicked the link to be authorized again.
+                //
+                if (Request.QueryString["reauth"] == "True")
+                {
+                    // Send an OpenID Connect sign-in request to get a new set of tokens.
+                    // If the user still has a valid session with Azure AD, they will not be prompted for their credentials.
+                    // The OpenID Connect middleware will return to this controller after the sign-in response has been handled.
+                    //
+                    HttpContext.GetOwinContext().Authentication.Challenge(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                }
+                //
+                // The user needs to re-authorize.  Show them a message to that effect.
+                //
+                ViewBag.ErrorMessage = "AuthorizationRequired";
+                return View();
+            }
+
+            //Setup GRaph API connection and get a list of roles
+            Guid ClientRequestId = Guid.NewGuid();
+            GraphSettings graphSettings = new GraphSettings();
+            graphSettings.ApiVersion = graphApiVersion;
+            GraphConnection graphConnection = new GraphConnection(accessToken, ClientRequestId, graphSettings);
+
+            PagedResults<Role> pagedResults = graphConnection.List<Role>(null, new FilterGenerator());
+
+            return View(pagedResults.Results);
+        }
+
+        /// <summary>
+        /// Gets details of a single <see cref="Role"/> Graph.
+        /// </summary>
+        /// <returns>A view with the details of a single <see cref="Role"/>.</returns>
+        public ActionResult Details(string objectId)
+        {
+            string accessToken = null;
+            string tenantId = ClaimsPrincipal.Current.FindFirst(TenantIdClaimType).Value;
+            if (tenantId != null)
+            {
+                accessToken = TokenCacheUtils.GetAccessTokenFromCacheOrRefreshToken(tenantId, graphResourceId);
+            }
+            if (accessToken == null)
+            {
+                //
+                // If refresh is set to true, the user has clicked the link to be authorized again.
+                //
+                if (Request.QueryString["reauth"] == "True")
+                {
+                    // Send an OpenID Connect sign-in request to get a new set of tokens.
+                    // If the user still has a valid session with Azure AD, they will not be prompted for their credentials.
+                    // The OpenID Connect middleware will return to this controller after the sign-in response has been handled.
+                    //
+                    HttpContext.GetOwinContext().Authentication.Challenge(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                }
+                //
+                // The user needs to re-authorize.  Show them a message to that effect.
+                //
+                ViewBag.ErrorMessage = "AuthorizationRequired";
+                return View();
+            }
+
+            // Setup Graph API connection and get single Role
+            Guid ClientRequestId = Guid.NewGuid();
+            GraphSettings graphSettings = new GraphSettings();
+            graphSettings.ApiVersion = graphApiVersion;
+            GraphConnection graphConnection = new GraphConnection(accessToken, ClientRequestId, graphSettings);
+
+            Role contact = graphConnection.Get<Role>(objectId);
+            return View(contact);
+        }
+
+        /// <summary>
+        /// Gets a list of <see cref="User"/> objects that are members of a give <see cref="Role"/>.
+        /// </summary>
+        /// <param name="objectId">Unique identifier of the <see cref="Role"/>.</param>
+        /// <returns>A view with the list of <see cref="User"/> objects.</returns>
+        public ActionResult GetMembers(string objectId)
+        {
+            string accessToken = null;
+            string tenantId = ClaimsPrincipal.Current.FindFirst(TenantIdClaimType).Value;
+            if (tenantId != null)
+            {
+                accessToken = TokenCacheUtils.GetAccessTokenFromCacheOrRefreshToken(tenantId, graphResourceId);
+            }
+            if (accessToken == null)
+            {
+                //
+                // If refresh is set to true, the user has clicked the link to be authorized again.
+                //
+                if (Request.QueryString["reauth"] == "True")
+                {
+                    // Send an OpenID Connect sign-in request to get a new set of tokens.
+                    // If the user still has a valid session with Azure AD, they will not be prompted for their credentials.
+                    // The OpenID Connect middleware will return to this controller after the sign-in response has been handled.
+                    //
+                    HttpContext.GetOwinContext().Authentication.Challenge(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                }
+                //
+                // The user needs to re-authorize.  Show them a message to that effect.
+                //
+                ViewBag.ErrorMessage = "AuthorizationRequired";
+                return View();
+            }
+            // Setup Graph API connection and get Role members
+            Guid ClientRequestId = Guid.NewGuid();
+            GraphSettings graphSettings = new GraphSettings();
+            graphSettings.ApiVersion = graphApiVersion;
+            GraphConnection graphConnection = new GraphConnection(accessToken, ClientRequestId, graphSettings);
+
+            Role role = graphConnection.Get<Role>(objectId);
+            IList<GraphObject> members = graphConnection.GetAllDirectLinks(role, LinkProperty.Members);
+
+            IList<User> users = new List<User>();
+
+            foreach (GraphObject obj in members)
+            {
+                if (obj is User)
+                {
+                    users.Add((User)obj);
+                }
+            }
+
+            return View(users);
+        }
+    }
+}
